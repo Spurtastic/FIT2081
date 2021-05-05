761fe1ba80747536f9351786d8d0c7e4deec2aea
Desk_Ash <asar0015@student.monash.edu>
2021-05-06 01:55:40 +0800

    done Week9

--- Current/CountryInfo_Student 2/app/src/main/java/edu/monash/fit2081/countryinfo/CountryDetails.java
+++ Current/CountryInfo_Student 2/app/src/main/java/edu/monash/fit2081/countryinfo/CountryDetails.java
@@ -1,5 +1,8 @@
 package edu.monash.fit2081.countryinfo;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import androidx.core.app.NavUtils;
@@ -8,21 +11,38 @@ import android.util.JsonReader;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
 
 import javax.net.ssl.HttpsURLConnection;
 
 public class CountryDetails extends AppCompatActivity {
-
+    // text views
     private TextView name;
     private TextView capital;
     private TextView code;
     private TextView population;
     private TextView area;
+    private TextView currency;
+    private TextView language;
+
+    // String items
+    private String languages;
+    private String wikiUrl;
+
+    // Image view
+    private ImageView flag;
+
+    // Button
+    private Button btnWiki;
     @Override
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,10 +59,22 @@ public class CountryDetails extends AppCompatActivity {
         code =  findViewById(R.id.country_code);
         population =  findViewById(R.id.population);
         area = findViewById(R.id.area);
+        currency = findViewById(R.id.currency_id);
+        language = findViewById(R.id.languages);
+        flag = findViewById(R.id.flagView);
+        btnWiki = findViewById(R.id.wiki_button);
+        btnWiki.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent wiki = new Intent(getApplicationContext(), WebWiki.class);
+                wiki.putExtra("key1", wikiUrl);
+                startActivity(wiki);
+
+            }
+        });
 
         new GetCountryDetails().execute(selectedCountry);
     }
-
 
     private class GetCountryDetails extends AsyncTask<String, String, CountryInfo> {
 
@@ -72,6 +104,7 @@ public class CountryDetails extends AppCompatActivity {
                         jsonReader.beginObject(); //consume object's opening JSON brace
                         while (jsonReader.hasNext()) {// process key/value pairs inside the current object
                             keyName = jsonReader.nextName();
+
                             if (keyName.equals("name")) {
                                 countryInfo.setName(jsonReader.nextString());
                                 if (countryInfo.getName().equalsIgnoreCase(selectedCountry)) {
@@ -85,7 +118,64 @@ public class CountryDetails extends AppCompatActivity {
                                 countryInfo.setPopulation(jsonReader.nextInt());
                             } else if (keyName.equals("area")) {
                                 countryInfo.setArea(jsonReader.nextDouble());
-                            } else {
+                            } else if (keyName.equals("currencies")) {
+                                jsonReader.beginArray();
+                                {
+                                    while(jsonReader.hasNext())
+                                    {
+                                        jsonReader.beginObject();
+                                        {
+                                            while(jsonReader.hasNext())
+                                            {
+
+                                                keyName = jsonReader.nextName();
+                                                if(keyName.equals("name"))
+                                                {
+                                                    countryInfo.setCurrency(jsonReader.nextString());
+                                                }
+                                                else{
+                                                    jsonReader.skipValue();
+                                                }
+                                            }
+                                        }
+                                        jsonReader.endObject();
+                                    }
+                                }
+                                jsonReader.endArray();
+
+                            }
+                            else if (keyName.equals("languages")) {
+                                languages  ="";
+                                jsonReader.beginArray();
+                                {
+                                    while(jsonReader.hasNext())
+                                    {
+                                        jsonReader.beginObject();
+                                        {
+                                            while(jsonReader.hasNext())
+                                            {
+
+                                                keyName = jsonReader.nextName();
+                                                if(keyName.equals("name"))
+                                                {
+                                                    languages += ", ";
+                                                    languages+= jsonReader.nextString();
+                                                    countryInfo.setLanguages(languages.substring(2));
+                                                }
+                                                else{
+                                                    jsonReader.skipValue();
+                                                }
+                                            }
+                                        }
+                                        jsonReader.endObject();
+                                    }
+                                }
+                                jsonReader.endArray();
+                            }
+                            else if (keyName.equals("alpha2Code")){
+                                countryInfo.setAlpha2Code(jsonReader.nextString());
+                            }
+                            else {
                                 jsonReader.skipValue();
                             }
                         }
@@ -111,7 +201,38 @@ public class CountryDetails extends AppCompatActivity {
             code.setText(countryInfo.getAlpha3Code());
             population.setText(Integer.toString(countryInfo.getPopulation()));
             area.setText(Double.toString(countryInfo.getArea()));
+            currency.setText(countryInfo.getCurrency());
+            language.setText(countryInfo.getLanguages());
+            btnWiki.setText("Wiki "+countryInfo.getName());
+            wikiUrl = "https://en.wikipedia.org/wiki/"+countryInfo.getName();
 
+            new GetFlag().execute("https://www.countryflags.io/"+countryInfo.getAlpha2Code()+"/flat/64.png");
+
+
+
+        }
+    }
+    private class GetFlag extends AsyncTask<String, Integer, Bitmap> {
+        @Override
+        protected Bitmap doInBackground(String... strings) {
+            try {
+                java.net.URL url = new java.net.URL(strings[0]);
+                HttpURLConnection connection = (HttpURLConnection) url
+                        .openConnection();
+                connection.setDoInput(true);
+                connection.connect();
+                InputStream input = connection.getInputStream();
+                Bitmap myBitmap = BitmapFactory.decodeStream(input);
+
+                return myBitmap;
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            flag.setImageBitmap(bitmap);
         }
     }
 
@@ -135,9 +256,33 @@ public class CountryDetails extends AppCompatActivity {
     private class CountryInfo {
         private String name;
         private String alpha3Code;
+        private String alpha2Code;
         private String capital;
         private int population;
         private double area;
+        private String currency;
+        private String languages;
+
+
+        public String getAlpha2Code() {
+            return alpha2Code;
+        }
+
+        public void setAlpha2Code(String alpha2Code) {
+            this.alpha2Code = alpha2Code;
+        }
+
+        public String getLanguages() {
+            return languages;
+        }
+
+        public void setLanguages(String languages) {
+            this.languages = languages;
+        }
+
+        public void setCurrency(String currency) {this.currency = currency;}
+
+        public String getCurrency() {return currency;}
 
         public String getName() {
             return name;
